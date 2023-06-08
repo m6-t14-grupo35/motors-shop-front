8e042607c4f3816daa94f62b1ff3dd46da6afee9
ismaelSS <ismael1337sn@gmail.com>
2023-06-08 13:37:55 -0300

    feat: create provisional navigate ba X developing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { Banner } from '@/components/banner'
 import { FooterComponete } from '@/components/footer'
+import { NavigateBar } from '@/components/navigateBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,7 +11,8 @@ export default function Home() {
 
    <>
    <Banner/>
-   <div className='bg-blue-500 h-[1500px]'></div>
+   <div className='bg-blue-500 h-[500px]'></div>
+   <NavigateBar page={2} count={50}/>
    <FooterComponete/>
    </>
   )
