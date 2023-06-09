a39d9e58e6d8dd296cc1e9b39a44cfad838747af
CaioFragoso23 <caiocesarfragoso@gmail.com>
2023-06-09 06:16:42 -0300

    feat/Ad Card stylization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { Banner } from '@/components/banner'
 import { ButtonFilter } from '@/components/buttonFilter'
+import { AdCard } from '@/components/adCard'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,7 +10,14 @@ export default function Home() {
   return (
    <>
     <Banner/>
+    <div className='flex flex-row-reverse'>
+    <div className='flex flex-row flex-wrap gap-12 mt-[54px]'>
+      <AdCard/>
+      <AdCard/>
+      <AdCard/>
+    </div>
     <ButtonFilter/>
+    </div>
    </>
   )
 }
