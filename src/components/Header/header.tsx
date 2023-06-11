90bedaf9fe22f5bcff7b6d1404b6b9826ae34ca5
CaioFragoso23 <caiocesarfragoso@gmail.com>
2023-06-11 20:40:26 -0300

    fixing hamburger stylization

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -13,7 +13,7 @@ const Header = (isLoggedIn: boolean, user: IUser) => {
     <>
       <header
         id="header"
-        className="flex-column sm:flex-row fixed top-0 left-0 h-20 z-10 bg-white w-screen flex align-center justify-between border-b-2 grey-6 px-16 flex-wrap"
+        className="flex-column sm:flex-row fixed top-0 left-0 h-20 z-30 bg-white w-screen flex align-center justify-between border-b-2 grey-6 px-16 flex-wrap"
       >
         <Image src={"/Logo.svg"} width={153} height={52} alt="Motors Shop" />
         <div className="flex items-center flex-column sm:flex-row flex-wrap">
@@ -23,34 +23,42 @@ const Header = (isLoggedIn: boolean, user: IUser) => {
               setToggle(!toggle);
               const header = document.getElementById("header");
               toggle
-                ? header?.classList.add("h-50") &&
+                ? header?.classList.add("sm:h-20", "h-screen") &&
                   header.classList.remove("h-20")
-                : header?.classList.remove("h-50") &&
+                : header?.classList.remove("sm:h-20", "h-screen") &&
                   header?.classList.add("h-20");
               const hamburger = document.getElementById("menuIcon");
               toggle
                 ? hamburger?.classList.add(
-                    "before:-rotate-45",
-                    "after:rotate-45"
+                    "before:rotate-45",
+                    "after:-rotate-45",
+                    "before:translate-y-0",
+                    "after:translate-y-0",
+                    "bg-white",
                   ) &&
                   hamburger?.classList.remove(
                     "before:-translate-y-2",
-                    "after:traslate-y-2"
+                    "after:translate-y-2",
+                    "bg-black"
                   )
                 : hamburger?.classList.remove(
-                    "before:-rotate-45",
-                    "after:rotate-45"
+                    "before:rotate-45",
+                    "after:-rotate-45",
+                    "before:translate-y-0",
+                    "after:translate-y-0",
+                    "bg-white",
                   ) &&
                   hamburger?.classList.add(
                     "before:-translate-y-2",
-                    "after:traslate-y-2"
+                    "after:translate-y-2",
+                    "bg-black"
                   );
               const nav = document.getElementById("navOptions");
               toggle
-                ? nav?.classList.remove("hidden", "border-l-2", "grey-6") &&
-                  nav?.classList.add("flex", "w-screen")
-                : nav?.classList.add("hidden", "border-l-2", "grey-6") &&
-                  nav?.classList.remove("flex", "w-screen");
+                ? nav?.classList.remove("hidden", "border-l-2", "grey-6","space-x-11") &&
+                  nav?.classList.add("flex")
+                : nav?.classList.add("hidden", "border-l-2", "grey-6","space-x-11") &&
+                  nav?.classList.remove("flex");
             }}
             className="
           sm:hidden
@@ -86,7 +94,7 @@ const Header = (isLoggedIn: boolean, user: IUser) => {
           className="hidden flex-col sm:flex sm:flex-row border-l-2 grey-6 sm:items-center space-x-11 pl-11"
         >
           <button className="body-1-600 text-grey-2">Fazer Login</button>
-          <button className="w-full body-1-600 border-grey-0 rounded border h-12 py-3 px-7 text-grey-0">
+          <button className="w-full body-1-600 border-grey-0 rounded border h-12 py-3 px-7 text-grey-0 ml-0">
             Cadastrar
           </button>
         </div>
