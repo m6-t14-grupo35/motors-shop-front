49fa35f81cfe09d921d57a3390cafda6354b44d3
Joice Blasi <joiceblasi@hotmail.com>
2023-06-12 13:40:51 -0300

    feat: scrool filter

--- src/components/buttonFilter/index.tsx
+++ src/components/buttonFilter/index.tsx
@@ -13,7 +13,7 @@ export const ButtonFilter = () => {
       <button className={`button-big-text text-grey-whiteFixed bg-brand-2 rounded w-[279px] h-12 cursor-pointer md:hidden flex items-center justify-center ${isOpenFilter === false ? 'flex' : 'hidden'}`} onClick={handleToggleFilter}>Filtros</button>
  
       <div className={`w-full md:flex ${isOpenFilter === true ? 'flex' : 'hidden'}`}>
-        <div className='flex flex-col max-h-[100rem] overflow-x-auto scroll-smooth items-start w-full max-w-sm px-3 py-3'>
+        <div className='flex flex-col max-h-[100rem]  overflow-x-auto scroll-smooth items-start w-full max-w-sm px-3 py-3'>
             <Filter toggleFilter={handleToggleFilter} />
         </div>
       </div>
