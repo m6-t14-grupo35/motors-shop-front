0932972aa8e4ad1e2709603720520fd5468fb1fc
CaioFragoso23 <caiocesarfragoso@gmail.com>
2023-06-23 10:17:44 -0300

    feat/admin page stylization w/ register ad modal integration

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,16 +1,23 @@
 import Header from "@/components/Header/header";
+import { IstateProvisorio } from "@/interfaces/componentProps.interface";
+import { ModalAddAd } from "@/components/modals/addAd";
+import { useState } from "react";
 import { FooterComponete } from "@/components/footer";
-import { ListCard } from "@/components/listCard";
-import { ProfileListCard } from "@/components/userListCard";
+import { AdminListCard } from "@/components/adminListCard";
 
 const user = {
   name: "Caio",
   email: "caio@mail.com",
 };
 
-export default function User() {
+
+export default function Admin() {
+  const [modal, setModal] = useState<IstateProvisorio>(null)
   return (
     <div className="bg-grey-8">
+      <>
+        {modal === "add" && <ModalAddAd closeFunction={() => setModal(null)}/>}
+      </>
       <Header isLoggedIn={true} user={user} />
       <div className="bg-brand-1 w-screen h-[357px]"></div>
 
@@ -20,7 +27,6 @@ export default function User() {
             <div className="w-[104px] h-[104px] rounded-full bg-brand-1 flex justify-center items-center text-white text-4xl">
               A
             </div>
-
               <div className="flex">
                 <p className="heading-6-600">Anunciante Anônimo</p>
                 <h2 className="bg-brand-4 text-brand-1 rounded py-1 px-2 body-2-400">
@@ -32,11 +38,10 @@ export default function User() {
                 industry. Lorem Ipsum has been the industry's standard dummy
                 text ever since the 1500s
               </p>
-
+              <button onClick={() => setModal("add")} className="flex items-center justify-center w-[160px] h-[48px] py-[12px] px-[28px] border-brand-1 border-2 rounded text-brand-1 bg-white">Criar Anúncio</button>
           </div>
-          <div className="flex-col justify-center md:pl-[60px] w-screen gap-[40px] mx-[60px]">
-            <h1 className="heading-5-600 pl-[60px] md:pl-0 py-[100px]">Anúncios</h1>
-            <ProfileListCard />
+          <div className="flex-col justify-center md:pl-[60px] w-screen gap-[40px] py-[50px] mx-[60px]">
+            <AdminListCard/>
           </div>
           <FooterComponete />
         </div>
