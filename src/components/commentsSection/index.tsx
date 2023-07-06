9a9eec575ec3a928cdf4243b28eebae50b775c4d
Joice Blasi <joiceblasi@hotmail.com>
2023-07-06 11:32:17 -0300

    fix: comments section id user

--- src/components/commentsSection/index.tsx
+++ src/components/commentsSection/index.tsx
@@ -1,13 +1,15 @@
 import { IcommentProps } from "@/interfaces/componentProps.interface"
 import { CommentComponent } from "../comment"
+import { parseCookies } from "nookies"
 
 export const CommentsSection = ({comments}:{comments:IcommentProps[]}) => {
+  const userIdCookie = parseCookies().motorsShopIdUser
   return(
     <div  className="flex flex-col gap-[24px] bg-white py-[36px] px-[34px] lg:px-[44px] rounded max-w-[751px]">
 
     <h2 className="heading-6-600 text-grey-1">Comentários</h2>
     <div className="flex flex-col gap-[44px]">
-      {comments.map((comment, index) => <CommentComponent key={`comment${index}`} userName={comment.userName} date={comment.date} text={comment.text}/>)}
+      {comments.map((comment, index) => <CommentComponent key={`comment${index}`} userName={comment.userName} date={comment.date} text={comment.text} userId={userIdCookie}/>)}
     </div>
   </div>
   )
