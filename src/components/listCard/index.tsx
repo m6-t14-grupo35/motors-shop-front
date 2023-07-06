335a6ac6ee9cb39d15caccbce9621bac0d36d4e5
Joice Blasi <joiceblasi@hotmail.com>
2023-07-06 12:28:40 -0300

    fix: ts-ignore

--- src/components/listCard/index.tsx
+++ src/components/listCard/index.tsx
@@ -40,7 +40,9 @@ export const ListCard = () => {
     <div className="my-2 overflow-x-auto mx-[60px] mb-[80px]">
       <div className="flex md:flex-wrap gap-12 md:justify-normal transition-all">
 
-        <AdCard is_sold={true} price={80000} priceFIPE={90000} brand={"Chevette"} model={"SL"} year={"1988"} fuel={"Gasolina"} km={20000} color={"vermelho"} description={"O carro mais desejado dos 90's em perfeito estado."} user_id={"2352"} created_at={"2023-01-01"} />
+        <AdCard 
+        //@ts-ignore
+        is_sold={true} price={80000} priceFIPE={90000} brand={"Chevette"} model={"SL"} year={"1988"} fuel={"Gasolina"} km={20000} color={"vermelho"} description={"O carro mais desejado dos 90's em perfeito estado."} user_id={"2352"} created_at={"2023-01-01"} />
         
       </div>
     </div>
